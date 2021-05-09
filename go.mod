d0f02618f2e5edc76760ec8a8016af65daf42da4
Scaffolder <scaffolder@backstage.io>
2021-05-09 12:30:19 +1000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/backstage/GoApp
+
+require (
+	
+	github.com/spf13/cobra v0.0.3
+	
+)
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	
+	"github.com/backstage/GoApp/cmd"
+)
+
+func main() {
+
+    
+    cmd.Execute()
+	
+}
